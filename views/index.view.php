d6529297c3395944dccb9b41eca5eb8526f91d5f
Wilfredo Meneses <meneseswill@gmail.com>
2019-06-17 18:41:41 -0600

    Crear la plantilla

--- views/index.view.php
+++ views/index.view.php
@@ -1,23 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Tareas para hoy</h1>
-    <ul>
-        <?php foreach($tasks as $task): ?>
-            <li>
-                <?php if ($task->completed): ?>
-                    <strike> <?= $task->description; ?> </strike>
-                <?php else: ?>
-                    <?= $task->description; ?>
-                <?php endif ?>
-            </li>          
-        <?php endforeach ?>
-    </ul>         
-</body>
-</html>
+<?php require 'partials/head.php'; ?>
+<h1>Tareas para hoy</h1>
+<ul>
+    <?php foreach($tasks as $task): ?>
+        <li>
+            <?php if ($task->completed): ?>
+                <strike> <?= $task->description; ?> </strike>
+            <?php else: ?>
+                <?= $task->description; ?>
+            <?php endif ?>
+        </li>          
+    <?php endforeach ?>
+</ul>         
+<?php require 'partials/footer.php'; ?>
---
+++ views/partials/nav.php
@@ -0,0 +1,8 @@
+<nav>
+    <ul>
+        <li><a href="/">Inicio</a></li>
+        <li><a href="/about">Acerca de</a></li>
+        <li><a href="/about/culture">Nuestra Cultura</a></li>  
+        <li><a href="/contact">Contacto</a></li>
+    </ul>
+</nav>
