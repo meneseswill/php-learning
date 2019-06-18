9438c049fa33a448a9a5fcb3098106b635b36e26
Wilfredo Meneses <meneseswill@gmail.com>
2019-06-17 21:43:37 -0600

    Autoload

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -4,10 +4,10 @@ $app = [];
 // $config = require 'config.php';
 $app['config'] = require 'config.php';
 
-require 'core/Router.php';
-require 'core/Request.php';
-require 'core/database/Connection.php';
-require 'core/database/QueryBuilder.php';
+// require 'core/Router.php';
+// require 'core/Request.php';
+// require 'core/database/Connection.php';
+// require 'core/database/QueryBuilder.php';
 
 $app['database'] = new QueryBuilder(
     Connection::make($app['config']['database'])
